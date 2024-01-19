93f6272e5dba19a055777664d76711d98bec02ec
stevendreed <steven.daniel.reed@gmail.com>
2024-01-19 12:21:19 -0800

    moved api into routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,20 @@
 // const express = require('express');
 import express from 'express';
 
+
+const mongoUrl = process.env.MONGO_URL;
+
 const app = express(); // init express router
 
 app.get('/dev', (req, res) => {
     res.json('testing hit the /dev route!');
 });
+
+// REGISTER route: route for NEW users
+app.post('register', (req, res) =>
+{
+
+})
 
 app.listen(4040, () =>
 {
