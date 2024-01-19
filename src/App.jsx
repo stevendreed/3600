c4b6ce2089fc71509582773c0428e4be70914996
stevendreed <steven.daniel.reed@gmail.com>
2024-01-19 10:58:53 -0800

    linked tailwindcss to src dir, tested basic import, installed autoprefixer (https://github.com/postcss/autoprefixer#readme)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-red-500">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
