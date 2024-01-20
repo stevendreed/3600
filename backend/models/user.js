ebfb7bc513bcfa19d0731c86ae4316b320f36133
stevendreed <steven.daniel.reed@gmail.com>
2024-01-19 16:19:12 -0800

    created userSchema for mongoose

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {type: String, unique:true}, // data type is string, force uniqueness
     password: String,
-});
+}, {timestamps: true}); // provides us a created-at timestamp
+
 
 module.exports = mongoose.model('user', userSchema);
