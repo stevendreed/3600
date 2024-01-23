30e54fd7791d6712ea044a29969e47999ef2b627
Mysmelf <matthewr9937@gmail.com>
2024-01-22 19:26:28 -0800

    Made significant changes to App.jsx file, including routing for JWT token verification to graphql and places for components to be rendered

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,31 @@ import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { useState } from 'react'
 
-// setting apolloclient to work with graphql
-const client = new ApolloClient({
+import { setContext } from '@apollo/client/link/context';
+import Header from './components/Header';
+import UserSidebar from './components/UserSidebar';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -15,8 +37,17 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
+      {/* main container that holds everything */}
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Outlet />
+        {/* header that appears in top right of webpage */}
+        <Header />
+        {/* UserSidebar appears on the left side of the screen */}
+        <UserSidebar />
+        {/* contains the content of the outlet */}
+        <div className="OutletContainer">
+          {/* Renders content depending on routing */}
+          <Outlet />
+        </div>
       </div>
     </ApolloProvider>
   )
