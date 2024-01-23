718c448c707886b7bd9b0d3aef77f8a21e3f048a
stevendreed <steven.daniel.reed@gmail.com>
2024-01-22 18:44:12 -0800

    added in subscription key to user class

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {type: String, unique:true}, // data type is string, force uniqueness
     password: String,
+    subscr_exp: Date, // date when paid subscription expires
 }, {timestamps: true}); // provides us a created-at timestamp
+
+// https://mongoosejs.com/docs/timestamps.html
+// use .createdAt, .updatedAt to access timestamps easily
 
 module.exports = mongoose.model('user', userSchema);
