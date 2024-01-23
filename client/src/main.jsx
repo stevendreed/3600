ed6f67b7b2e5c938b694caee4c941f37820b4f2a
Mysmelf <matthewr9937@gmail.com>
2024-01-22 19:32:32 -0800

    Created files for components: Chatroom, CreateRoomForm, Header, UserSidebar. Files are currently empty

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 // these pages need to be created and exported to properly render below
 import Home from './pages/Home';
 import Chatroom from './pages/Chatroom';
-import CreateRoom from './pages/CreateRoom';
+import CreateRoomForm from './pages/CreateRoomForm';
 import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter([
@@ -18,12 +18,15 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       {
+        // homepage functions also as the login page
         index: true,
         element: <Home />
       }, {
+        // page where logged in users can create a room
         path: '/CreateRoom',
-        element: <CreateRoom />
+        element: <CreateRoomForm />
       }, {
+        // specific chatroom a user has joined
         // chatroom uses a wildcard in its parameters to match the user with the correct database information
         path: '/Chatroom/:id',
         element: <Chatroom />
