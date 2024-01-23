bf24e459bc357483fabf4ed233c028e05ab05fc1
Mysmelf <matthewr9937@gmail.com>
2024-01-22 18:50:13 -0800

    Created full setup for main.jsx routing with rooms: Home, CreateRoom, and Chatroom + an error message. These pages are not currently created, but will be.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
-
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.jsx'
 import './index.css'
 
+import App from './App.jsx'
+// these pages need to be created and exported to properly render below
+import Home from './pages/Home';
+import Chatroom from './pages/Chatroom';
+import CreateRoom from './pages/CreateRoom';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/CreateRoom',
+        element: <CreateRoom />
+      }, {
+        // chatroom uses a wildcard in its parameters to match the user with the correct database information
+        path: '/Chatroom/:id',
+        element: <Chatroom />
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
