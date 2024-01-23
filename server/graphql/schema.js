a911f5380dd6d97616d444642d904745955b068b
astro <angelica.strong0725@gmail.com>
2024-01-22 21:25:51 -0800

    resolved changes in pr

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -5,19 +5,13 @@ const { User, Message, Chatroom } = require("../models");
 const typeDefs = gql`
   type Query {
     user: User
-    messages(_id: ID!): [Message]
+    messages(chatroomId: ID!): [Message]
     chatrooms: [Chatroom]
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    addMessage(
-      sender: String!
-      message_id: UUID!
-      content: String!
-      thread: UUID!
-      location: UUID!
-    ): Message
+    addMessage(sender: ID!, content: String!, thread: ID, location: ID!): Message
     updateUser(username: String!, email: String!, password: String!): User
     login(email: String!, password: String!): Auth
   }
@@ -25,18 +19,34 @@ const typeDefs = gql`
   type User {
     _id: ID
     username: String
-    email: String
-    password: String
   }
 
   type Message {
     _id: ID
-    sender: String
-    message_id: UUID
+    sender: User
     content: String
-    reaction: json
-    thread: UUID
-    location: UUID
+    reaction: String
+    thread: ID
+    location: Chatroom
+  }
+
+  type Chatroom {
+    _id: ID
+    title: String
+    tags: [Tag]
+    icon: String
+    activeUsers: [User]
+    createdAt: String
+  }
+
+  type Tag {
+    _id: ID
+    name: String
+  }
+
+  type Auth {
+    token: String
+    user: User
   }
 `;
 
