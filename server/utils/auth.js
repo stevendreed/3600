78eb6614fbcb5ef41a5ddce7824d888c9fa9732a
stevendreed <steven.daniel.reed@gmail.com>
2024-01-23 20:05:23 -0800

    added expiration time frame + explanation comments

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const secret = process.env.JWT_SECRET;
+const expiration = '1h'; // CHANGE THIS VALUE to update length of expiration
+// uses VERCEL/MS => https://github.com/vercel/ms
+// vercel is a way to convert various times into ms - quite handy!
+
