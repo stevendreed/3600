06e694a82bfbd48b01d97ecbf38c33e90066c830
Mysmelf <matthewr9937@gmail.com>
2024-01-23 19:00:00 -0800

    Created form states for login and signup forms

--- client/src/components/UserSidebar/index.jsx
+++ client/src/components/UserSidebar/index.jsx
@@ -2,8 +2,27 @@
 // it will either display a login forum or user information in a ternary depending on if
 // the state is set to logged in or not
 
+// useState is used to take forum information
+import { useState } from 'react';
+// useMutation is required on the front end to perform a mutation
+import { useMutation } from '@apollo/client';
+
+// We need mutations for logging in and adding users, here is an example of how it would be written:
+// import { LOGIN_USER } from '../utils/mutations';
+// import { ADD_USER } from '../utils/mutations';
+
 // function to export our sidebar content to Outlet in App.jsx based on url defined in Main.jsx
 const UserSidebar = () => {
+
+    // declaration for login and signup forms (when user not logged in)
+    // mutations need to be finished on the backend for front end implementation
+    const [loginFormState, setLoginFormState] = useState({ username: '', password: '' });
+    // const [login, { loginError, loginData }] = useMutation(LOGIN_USER);
+
+    const [signupFormState, setSignupFormState] = useState({ username: '', email: '', password: '' });
+    // const [signup, { signupError, signupData }] = useMutation(ADD_USER);
+
+
 
     // what we want to render
     return (
