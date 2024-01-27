0ab74f2fad9db02fca08503ad95c191e03b86a2c
Mysmelf <matthewr9937@gmail.com>
2024-01-27 12:27:59 -0800

    Began creating input form for CreateRoom Page

--- client/src/pages/CreateRoom.jsx
+++ client/src/pages/CreateRoom.jsx
@@ -1,17 +1,87 @@
 // will render the CreateRoomForm component
 
+import { useContext, createContext, useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { CREATE_ROOM } from '../../utils/mutations';
+
 const CreateRoom = () => {
     // This is an example of how we run our query through frontend
     // const { loading, data } = useQuery(QUERY_THOUGHTS);
 
     // if no data is returned, set thoughts to an empty array
     // const thoughts = data?.thoughts || [];
+
+    const [createRoomState, setCreateRoomFormState] = useState({ title: '', tags: [], icon: '' });
+    const [createRoom] = useMutation(CREATE_ROOM);
+
+    const handleCreateRoomChange = (event) => {
+      const { name, value } = event.target;
+
+      setCreateRoomFormState({
+          ...createRoomState,
+          [name]: value,
+      });
+      };
+
+      const handleCreateRoomSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+            const { signupData } = await addUser({
+            variables: { ...signupFormState },
+            });
+    
+            Auth.login(signupData.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+
+        // clear signup form
+        setSignupFormState({
+            email: '',
+            username: '',
+            password: '',
+            });
+    };
   
     return (
       <div className="createRoom-container">
-        <div className="createRoomInner-container">
 
-        </div>
+          <form className='createRoom-form' onSubmit={handleSignupSubmit}>
+                        <h3>Sign Up</h3>
+                            <input
+                            className="signup-form-input"
+                            placeholder="Email"
+                            name="email"
+                            type="email"
+                            value={signupFormState.email}
+                            onChange={handleSignupChange}
+                            />
+                            <input
+                            className="signup-form-input"
+                            placeholder="Username"
+                            name="username"
+                            type="username"
+                            value={signupFormState.username}
+                            onChange={handleSignupChange}
+                            />
+                            <input
+                            className="signup-form-input"
+                            placeholder="Password"
+                            name="password"
+                            type="password"
+                            value={signupFormState.password}
+                            onChange={handleSignupChange}
+                            />
+                            <button
+                            className="submitButton"
+                            style={{ cursor: 'pointer' }}
+                            type="submit"
+                            >
+                            Submit
+                            </button>
+                        </form>
+
       </div>
     );
   };
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,3 +27,15 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const CREATE_ROOM = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
