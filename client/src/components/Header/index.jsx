2bd70b4e823618fbd419041e1320fb227be7578c
stevendreed <steven.daniel.reed@gmail.com>
2024-01-27 14:12:11 -0800

    updated header.index file with standard coding style

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,33 +4,33 @@
 
 import queries from '../../utils/queries';
 
+
 // function to export our header content to Outlet in App.jsx based on url defined in Main.jsx
 const Header = () => {
 
-    // what we want to render
-    return (
-        <div className="header">
-            <h1>3600</h1>
-            <p>One Hour Chatrooms</p>
+  // what we want to render
+  return (
+    <div className="header">
+      <h1>3600</h1>
+      <p>One Hour Chatrooms</p>
 
-            {/* room sorting dropdown menu */}
-            <div className="sortDropdown">
-                <button>Sort Rooms By...</button>
-                <div className="dropdownOptions">
-                    {/* sort rooms by active users */}
-                    <button id="sortbyUsers" onClick={function () {
+      {/* room sorting dropdown menu */}
+      <div className="sortDropdown">
+        <button>Sort Rooms By...</button>
+        <div className="dropdownOptions">
+        {/* sort rooms by active users */}
+          <button id="sortbyUsers" onClick={function () {
                         
-                    }}>Most Users</button>
-                    {/* sort rooms old to new */}
-                    <button id="sortbyOld">Oldest</button>
-                    {/* sort rooms new to old */}
-                    <button id="sortbyNew">Newest</button>
-                </div>
-            </div>
-
+          }}>Most Users</button>
+          {/* sort rooms old to new */}
+          <button id="sortbyOld">Oldest</button>
+              {/* sort rooms new to old */}
+          <button id="sortbyNew">Newest</button>
         </div>
-    )
-    
-    }
-    
+      </div>
+    </div>
+  );
+
+}
+
     export default Header
