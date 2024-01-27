8c2a05a4212c906367730daa5c528b2b3c26e54c
Mysmelf <matthewr9937@gmail.com>
2024-01-27 12:53:45 -0800

    Created desktop layout for 'CreateRoom' page

--- client/src/pages/CreateRoom.jsx
+++ client/src/pages/CreateRoom.jsx
@@ -2,7 +2,7 @@
 
 import { useContext, createContext, useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { CREATE_ROOM } from '../../utils/mutations';
+import { CREATE_ROOM } from '../utils/mutations';
 
 const CreateRoom = () => {
     // This is an example of how we run our query through frontend
@@ -27,58 +27,69 @@ const CreateRoom = () => {
         event.preventDefault();
     
         try {
-            const { signupData } = await addUser({
-            variables: { ...signupFormState },
+            const { createRoomData } = await createRoom({
+            variables: { ...createRoomState },
             });
     
-            Auth.login(signupData.addUser.token);
         } catch (e) {
             console.error(e);
         }
 
         // clear signup form
-        setSignupFormState({
-            email: '',
-            username: '',
-            password: '',
+        setCreateRoomFormState({
+            title: '',
+            tags: [],
+            image: '',
             });
     };
   
     return (
       <div className="createRoom-container">
 
-          <form className='createRoom-form' onSubmit={handleSignupSubmit}>
-                        <h3>Sign Up</h3>
+          <form className='createRoom-form' onSubmit={handleCreateRoomSubmit}>
+                        <h3>Create Your Room</h3>
                             <input
-                            className="signup-form-input"
-                            placeholder="Email"
-                            name="email"
-                            type="email"
-                            value={signupFormState.email}
-                            onChange={handleSignupChange}
+                            className="createRoom-form-input"
+                            placeholder="Room Name"
+                            name="title"
+                            type="title"
+                            value={createRoomState.title}
+                            onChange={handleCreateRoomChange}
                             />
                             <input
-                            className="signup-form-input"
-                            placeholder="Username"
-                            name="username"
-                            type="username"
-                            value={signupFormState.username}
-                            onChange={handleSignupChange}
+                            className="createRoom-form-input"
+                            placeholder="Room Tags"
+                            name="tags"
+                            type="tags"
+                            value={createRoomState.tags}
+                            onChange={handleCreateRoomChange}
                             />
                             <input
-                            className="signup-form-input"
-                            placeholder="Password"
-                            name="password"
-                            type="password"
-                            value={signupFormState.password}
-                            onChange={handleSignupChange}
+                            className="createRoom-form-input"
+                            placeholder="Pick an Icon"
+                            name="image"
+                            type="icon"
+                            value={createRoomState.image}
+                            onChange={handleCreateRoomChange}
                             />
+                            <div className='imageChoice-container'>
+                              {/* images user can select, attach function onclick set createRoomState.image to the url */}
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                              <img className='imageChoice' src='/images/profileimgexample.jpg'></img>
+                            </div>
                             <button
-                            className="submitButton"
+                            className="roomSubmitButton"
                             style={{ cursor: 'pointer' }}
                             type="submit"
                             >
-                            Submit
+                            Create Room
                             </button>
                         </form>
 
