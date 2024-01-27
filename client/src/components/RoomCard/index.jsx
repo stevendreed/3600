d74ec95e2e30ae390476f1b606cb2a8e1aeba8eb
stevendreed <steven.daniel.reed@gmail.com>
2024-01-26 17:49:03 -0800

    updated RoomCard component with basic events

--- client/src/components/RoomCard/index.jsx
+++ client/src/components/RoomCard/index.jsx
@@ -5,14 +5,43 @@ const RoomCard = (item) => {
 
 // what we want to render
 return (
-    <div className="card">
-        <img src={`/images/${item.image}`}></img>
-        <h1>{item.title}</h1>
-        <h2>{item.tags}</h2>
-        <h3>{item.timer}</h3>
-    </div>
+  <div className="card">
+      <img src={`/images/${item.image}`}></img>
+    {/* <h1>
+        {item.title}
+    </h1> 
+    <h2>{item.tags}</h2>
+    <h3>{item.timer}</h3>*/}
+    <button onClick={function handleClick() {
+        console.log(`RoomCard -> title was clicked`);
+        // TASKS:
+
+        // IF: user => redirect to chatroom
+
+        // IF: !user => "you must sign in" modal/alert
+    }}>
+        {item.title}
+    </button>
+    <button onClick={function handleClick() {
+        console.log(`RoomCard -> tags was clicked`);
+        // TASKS
+        // IF: owner of room => allow adding tags
+
+        // IF: !owner => display tags w/o editing ability
+    }}>
+        {item.tags}
+    </button>
+    <button onClick={function handleClick() {
+        console.log(`RoomCard -> timer was clicked`);
+        // not sure if we want functionality here, but if
+        // all of the el on each card is a button it makes
+        // styling them easier :)
+    }}>
+        {item.timer}
+    </button>
     
-)
+  </div>    
+);
 
 }
 
