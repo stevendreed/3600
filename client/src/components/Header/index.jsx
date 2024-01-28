cbdd219c3348e00ea1ac850c73b0bc78dff930ce
stevendreed <steven.daniel.reed@gmail.com>
2024-01-28 14:47:11 -0800

    updated 4sp tabs to 2sp

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,42 +1,46 @@
 // this will be the header, and is rendered on each page
-// will contain ternaries depending on url to render either siteinfo or room info
+// will contain ternaries depending on url to render either siteinfo or room
+// info
 // (Like room title, timer, ect...)
 
 import Home from '../../pages/Home'
 import { useNavigate } from 'react-router-dom'
 
-// function to export our header content to Outlet in App.jsx based on url defined in Main.jsx
+// function to export our header content to Outlet in App.jsx based on url
+// defined in Main.jsx
 const Header = () => {
-    let navi = useNavigate();
-    const reroute = (PATH) => {
-        navi(PATH);
-    } 
+  // sets a reroute hook in this component. Pass in PATH argument to reroute
+  // the user to the desired URL (based on deployment link/port)
+  let navi = useNavigate();
+  const reroute = (PATH) => {
+    navi(PATH);
+  } 
   // what we want to render
   return (
-    <div className='header'>
-      <button onClick={function() {
-        reroute('/');
-      }
-      }>3600
-      </button>
-      <p>One Hour Chatrooms</p>
-
-      {/* room sorting dropdown menu */}
-      <div className='sortDropdown'>
-        <button>Sort Rooms By...</button>
-          <div className='dropdownOptions'>
-            {/* sort rooms by active users */}
-            <button id='sortbyUsers'>Most Users</button>
-            {/* sort rooms old to new */}
-            <button id='sortbyOld'>Oldest</button>
-            {/* sort rooms new to old */}
-            <button id='sortbyNew'>Newest</button>
-          </div>
-      </div>
-
-    </div>
-    );
-    
+  <div className='header'>
+    <button onClick={function() {
+    reroute('/');
     }
-    
-    export default Header
+    }>3600
+    </button>
+    <p>One Hour Chatrooms</p>
+
+    {/* room sorting dropdown menu */}
+    <div className='sortDropdown'>
+    <button>Sort Rooms By...</button>
+      <div className='dropdownOptions'>
+      {/* sort rooms by active users */}
+      <button id='sortbyUsers'>Most Users</button>
+      {/* sort rooms old to new */}
+      <button id='sortbyOld'>Oldest</button>
+      {/* sort rooms new to old */}
+      <button id='sortbyNew'>Newest</button>
+      </div>
+    </div>
+
+  </div>
+  );
+  
+  }
+  
+  export default Header
