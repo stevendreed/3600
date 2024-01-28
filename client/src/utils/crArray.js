8e2af2dfb2094d6830b76847c90a67da87f55c33
stevendreed <steven.daniel.reed@gmail.com>
2024-01-28 15:57:38 -0800

    added basic query logic

--- client/src/utils/crArray.js
+++ client/src/utils/crArray.js
@@ -5,28 +5,36 @@
 // chatrooms & returns this array of objects ordered per parameters
 //
 // PARAMETERS
-// sortBy => a key-value pair, with the following list of supported values:
-// sortBy: "oldest", "newest", "userCount"
+// sortBy => a string, with the following list of supported values:
+// "oldest", "newest", "userCount"
 //
 
 
 // imports
-
-
+import { gql } from '@apollo/client';
 
 // function prototype
 const getCRArray = async function(sortBy)
 {
   // 1: initialize local scope variables:
+  //
   // crArray = an array of objects. Each element is chatroom data with
   // { _id, title, owner, tags, icon, createdAt }
-  // mutation = a string that is passed to the GraphQL Apollo server s.t.
-  // 
-
   let crArray = [];
+  // mutation = a string that is passed to the GraphQL Apollo server s.t.
+
+  const query = {
+    oldest: gql``,
+    newest: gql``,
+    userCount: gql``
+  }
 
   // 2: query apollo server for ALL chatrooms with 
   // createdAt values > Date.now() - (60 * 60 * 1000)
+  
+  const { data } = await client.query({
+    query: query.sortBy,
+  });
 
   // 3: sort the incoming data (either in the mutation or client-side) by the 
   // sortBy value (oldest, newest, userCount)
