7d819c81aaff344a622c7ef8e5082ca19dadb16d
stevendreed <steven.daniel.reed@gmail.com>
2024-01-28 14:13:41 -0800

    added loggedIn verification for RoomCard->title click

--- client/src/components/RoomCard/index.jsx
+++ client/src/components/RoomCard/index.jsx
@@ -1,4 +1,5 @@
 // this will will be the individual cards rendered on the home page for each chat room
+import AuthService from '../../utils/auth';
 
 // function to export our homepage content to Outlet in App.jsx based on url defined in Main.jsx
 const RoomCard = (item) => {
@@ -8,25 +9,34 @@ return (
   <div className="card">
       <img src={`/images/${item.image}`}></img>
     {/* <h1>
-        {item.title}
+      {item.title}
     </h1> 
     <h2>{item.tags}</h2>
     <h3>{item.timer}</h3>*/}
     <button onClick={function handleClick() {
-        console.log(`RoomCard -> title was clicked`);
-        // TASKS:
+      console.log(`RoomCard -> title was clicked`);
+      // TASKS:
 
-        // IF: user => redirect to chatroom
+      // IF: user => redirect to chatroom
+      if (AuthService.loggedIn())
+      {
+        console.log('User is logged in');
+      }
+      // exception handling to reject non-signed in interactions
+      else
+      {
+        console.log('User is not signed in');
+      }
 
-        // IF: !user => "you must sign in" modal/alert
-    }}>
+      // IF: !user => "you must sign in" modal/alert
+      }}>
         {item.title}
     </button>
     <button onClick={function handleClick() {
         console.log(`RoomCard -> tags was clicked`);
         // TASKS
         // IF: owner of room => allow adding tags
-
+         
         // IF: !owner => display tags w/o editing ability
     }}>
         {item.tags}
