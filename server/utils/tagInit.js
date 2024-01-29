7294203aa434ee74d898a75018c74775c14880a5
astro <angelica.strong0725@gmail.com>
2024-01-28 20:08:44 -0800

    refactored code

--- server/utils/tagInit.js
+++ server/utils/tagInit.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-const { Tag } = require('../models/tags');
-const tagNames = require('./tagList');
+const db = require('./models'); 
+const tagNames = require('./tagList'); 
 
 const tagUri = 'mongodb://localhost:27017/Cluster0';
 
@@ -9,29 +8,20 @@ mongoose.connect(tagUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB successfully connected on port 27017"))
     .catch(err => console.error(`MongoDB connection error: ${err}`));
 
-// function to save tags to the database
-async function saveTagsToDB() {
+const initializeTags = async () => {
   try {
-    // loop through the tagNames array and save each tag to the database
     for (const tagName of tagNames) {
-      const existingTag = await Tag.findOne({ name: tagName });
-      if (!existingTag) {
-        const tag = new Tag({ name: tagName });
-        await tag.save();
-        console.log(`Tag saved: ${tagName}`);
-      } else {
-        console.log(`Tag already exists: ${tagName}`);
+      const tagExists = await db.Tags.findOne({ name: tagName });
+      if (!tagExists) {
+        const newTag = new db.Tags({ name: tagName });
+        await newTag.save();
+        console.log(`Added tag: ${tagName}`);
       }
     }
+    console.log('Tags initialization completed.');
   } catch (error) {
-    console.error(`Error saving tags to the database: ${error}`);
-  } finally {
-    // disconnect from MongoDB once tags are saved (optional)
-    mongoose.disconnect()
-      .then(() => console.log("MongoDB disconnected"))
-      .catch(err => console.error(`Error disconnecting MongoDB: ${err}`));
+    console.error('Error initializing tags:', error);
   }
-}
+};
 
-// call the function to save tags to the database
-saveTagsToDB();
+initializeTags().then(() => mongoose.disconnect());
