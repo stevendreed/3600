c5fb4a10410484e13ceb1041821cff8911681290
stevendreed <steven.daniel.reed@gmail.com>
2024-01-28 16:23:11 -0800

    added switch statement to choose which query to utilize when sorting chatrooms

--- client/src/utils/crArray.js
+++ client/src/utils/crArray.js
@@ -12,6 +12,7 @@
 
 // imports
 import { gql } from '@apollo/client';
+import { GET_ALL_CR } from './queries';
 
 // function prototype
 const getCRArray = async function(sortBy)
@@ -23,21 +24,44 @@ const getCRArray = async function(sortBy)
   let crArray = [];
   // mutation = a string that is passed to the GraphQL Apollo server s.t.
 
-  const query = {
-    oldest: gql``,
-    newest: gql``,
-    userCount: gql``
-  }
+  // const query = {
+  //   oldest: gql``,
+  //   newest: gql``,
+  //   userCount: gql``
+  // }
 
   // 2: query apollo server for ALL chatrooms with 
   // createdAt values > Date.now() - (60 * 60 * 1000)
   
+  // const { data } = await client.query({
+  //   query: query.sortBy,
+  // });
+
   const { data } = await client.query({
-    query: query.sortBy,
+    query: GET_ALL_CR
   });
 
   // 3: sort the incoming data (either in the mutation or client-side) by the 
   // sortBy value (oldest, newest, userCount)
+
+  switch (sortBy) {
+    case 'oldest':
+      crArray = data.chatrooms.slice().sort((a,b) => a.createdAt - b.createdAt);
+      break;
+
+    case 'newest':
+      crArray = data.chatrooms.slice().sort((a,b) => b.createdAt - a.createdAt);
+      break;
+
+    case 'userCount':
+      crArray = data.chatrooms.slice().sort((a,b) => b.userCount - a.userCount);
+      break;
+
+    default:
+      throw new Error ('ERROR: function getCRArray: invalid parameter')
+      break;
+
+  } // end switch 
 
   // 4: return the array of objects 
   return crArray;
