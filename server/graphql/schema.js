e04d5abd1b972a594a3c220c79432c8eac6989eb
astro <angelica.strong0725@gmail.com>
2024-01-28 16:34:29 -0800

    ensured that graphql reads writes n deletes to model db

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -6,6 +6,7 @@ const { signToken } = require('../utils/auth');
 // const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const db = require('../models');
 
 const typeDefs = gql`
   type Query {
@@ -84,22 +85,22 @@ const resolvers = {
   Query: {
     // fetch a user by ID
     user: async (_, args) => {
-      return await User.findById(args.id);
+      return await db.User.findById(args.id);
     },
     // fetch messages for a specific chatroom
     messages: async (_, { chatroomId }) => {
-      return await Message.find({ location: chatroomId });
+      return await db.Message.find({ location: chatroomId });
     },
     // fetch all chatrooms
     chatrooms: async () => {
-      return await Chatroom.find({}).populate('tags');
+      return await db.Chatroom.find({}).populate('tags');
     },
   },
   // mutation resolvers
   Mutation: {
     // user login
     LOGIN_USER: async (_, { email, password }, context) => {
-      const user = await User.findOne({ email });
+      const user = await db.User.findOne({ email });
       if (!user || !await user.isCorrectPassword(password)) {
         throw new Error('Invalid credentials');
       }
@@ -116,7 +117,7 @@ const resolvers = {
       // variable for hashing password
       const hashedPassword = await bcrypt.hash(password, saltRounds);
       // new user saved to database & hashes password
-      const newUser = new User({
+      const newUser = new db.User({
         username,
         email,
         password: hashedPassword
@@ -126,17 +127,14 @@ const resolvers = {
       return { token, user: newUser };
     },
     CHANGE_EMAIL: async (_, { userId, newEmail }, context) => {
-      // authenticate the user
       if (!context.user || context.user._id !== userId) {
         throw new Error('Unauthorized');
       }
-      // check if the new email is already in use
-      const emailExists = await User.findOne({ email: newEmail });
+      const emailExists = await db.User.findOne({ email: newEmail });
       if (emailExists) {
         return { success: false, message: 'Email already in use' };
       }
-      // find the user and update the email
-      const user = await User.findById(userId);
+      const user = await db.User.findById(userId);
       if (!user) {
         throw new Error('User not found');
       }
@@ -145,17 +143,14 @@ const resolvers = {
       return { success: true, message: 'Email successfully changed' };
     },
     CHANGE_USERNAME: async (_, { userId, newUsername }, context) => {
-      // authenticate the user (ensure they are who they claim to be)
       if (!context.user || context.user._id !== userId) {
         throw new Error('Unauthorized');
       }
-      // check if the new username is already in use
-      const usernameExists = await User.findOne({ username: newUsername });
+      const usernameExists = await db.User.findOne({ username: newUsername });
       if (usernameExists) {
         return { success: false, message: 'Username already in use' };
       }
-      // find the user and update the username
-      const user = await User.findById(userId);
+      const user = await db.User.findById(userId);
       if (!user) {
         throw new Error('User not found');
       }
@@ -164,37 +159,32 @@ const resolvers = {
       return { success: true, message: 'Username successfully changed' };
     },
     CHANGE_PASSWORD: async (_, { userId, oldPassword, newPassword }, context) => {
-      // authenticate the user 
       if (!context.user || context.user._id !== userId) {
         return { success: false, message: 'Unauthorized' };
       }
-      // find the user by ID
-      const user = await User.findById(userId);
+      const user = await db.User.findById(userId);
       if (!user) {
         return { success: false, message: 'User not found' };
       }
-      // check if the old password is correct
       const isMatch = await bcrypt.compare(oldPassword, user.password);
       if (!isMatch) {
         return { success: false, message: 'Old password is incorrect' };
       }
-      // hash the new password
       const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
-      // update the user's password
       user.password = hashedPassword;
       await user.save();
       return { success: true, message: 'Password successfully changed' };
     },
     DELETE_USER: async (_, context) => {
-      if (context.user)
-      {
-        return User.fineOneAndDelete({_id: context.user._id})
+      if (context.user) {
+        await db.User.findOneAndDelete({ _id: context.user._id });
+        return { success: true, message: 'User deleted' };
       }
       throw new Error('Could not find a user to delete');
     },
      // add a new message
     ADD_MESSAGE: async (_, { sender, content, thread, location }, context) => {
-      const newMessage = new Message({ sender, content, thread, location });
+      const newMessage = new db.Message({ sender, content, thread, location });
       await newMessage.save();
       return newMessage;
     },
@@ -203,7 +193,7 @@ const resolvers = {
       if (!context.user) {
         throw new Error('Authentication required');
       }
-      const message = await Message.findById(messageId);
+      const message = await db.Message.findById(messageId);
       if (!message) {
         throw new Error('Message not found');
       }
@@ -214,44 +204,36 @@ const resolvers = {
       await message.save();
       return message;
     },
-    DELETE_MESSAGE: async (_, context) => {
-      if (context.sender)
-      {
-        return Message.fineOneAndDelete({_id: context.message._id});
+    DELETE_MESSAGE: async (_, { messageId }, context) => {
+      if (context.user) {
+        await db.Message.findOneAndDelete({ _id: messageId });
+        return { success: true, message: 'Message deleted' };
       }
-      throw new Error('Could not find a user to delete');
+      throw new Error('Could not find a message to delete');
     },
     // add a new chatroom
     ADD_CHATROOM: async (_, { title, tagNames, icon }, context) => {
-      // find tags based on names
       const tags = await Promise.all(tagNames.map(async (name) => {
-        let tag = await Tag.findOne({ name });
+        let tag = await db.Tag.findOne({ name });
         if (!tag) {
-          tag = new Tag({ name });
+          tag = new db.Tag({ name });
           await tag.save();
         }
         return tag;
       }));
-  
-      // extract tag IDs
+
       const tagIds = tags.map(tag => tag._id);
-  
-      const newChatroom = new Chatroom({ title, tags: tagIds, icon });
+      const newChatroom = new db.Chatroom({ title, tags: tagIds, icon });
       await newChatroom.save();
       return newChatroom;
     },
-    DELETE_CHATROOM: async(_, context) => {
-      if (!context._id)
-      {
-        throw new Error('No chatroom found');
-      }
-      if (!context.user)
-      {
+    DELETE_CHATROOM: async (_, { chatroomId }, context) => {
+      if (!context.user) {
         throw new Error('You do not have permission to delete the chatroom');
       }
-      // verify the id for our discovered chatroom is not null
-      return Chatroom.findOneAndDelete({_id: context._id});
-    } // end deleteChatroom
+      await db.Chatroom.findOneAndDelete({ _id: chatroomId });
+      return { success: true, message: 'Chatroom deleted' };
+    },
   },
 };
 
