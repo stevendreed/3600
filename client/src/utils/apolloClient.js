dac2d9c06c4c99d6ee88db47a57ce8aca24538fc
stevendreed <steven.daniel.reed@gmail.com>
2024-01-30 14:58:18 -0800

    updated apolloClient with websocket fixes

--- client/src/utils/apolloClient.js
+++ client/src/utils/apolloClient.js
@@ -6,7 +6,9 @@ import {
 } from '@apollo/client';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { setContext } from '@apollo/client/link/context';
+import { createClient } from 'graphql-ws';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -22,12 +24,22 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:5173/graphql',
+// const wsLink = new WebSocketLink({
+//   uri: 'ws://localhost:5173/graphql',
+//   options: {
+//     reconnect: true,
+//   },
+// });
+
+// solution from 
+// https://community.apollographql.com/t/subscriptions-websocket-connection-to-
+// ws-localhost-4000-graphql-failed/3439/5
+const wsLink = new GraphQLWsLink(createClient({
+  uri: 'ws://localhost:5000/graphql',
   options: {
-    reconnect: true,
-  },
-});
+    reconnect: true
+  }
+}));
 
 const splitLink = split(
   ({ query }) => {
