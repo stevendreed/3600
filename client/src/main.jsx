1a96f5d9710cec268a831ef4b9404d20a53913e7
astro <angelica.strong0725@gmail.com>
2024-01-29 21:07:38 -0800

    created util for apollo client to setup react dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ApolloProvider } from '@apollo/client';
-import client from './apolloClient';
+import { client } from './utils/apolloClient.js';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css'
---
+++ client/src/utils/apolloClient.js
@@ -0,0 +1,37 @@
+// src/apolloClient.js
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  createHttpLink,
+  split,
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5000/graphql', 
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink),
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
