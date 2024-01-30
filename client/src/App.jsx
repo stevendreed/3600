87a904f79efaa965c8462ddba64acc53840447d8
astro <angelica.strong0725@gmail.com>
2024-01-29 20:50:31 -0800

    setup websocket in front end for subs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,11 @@ import {
   ApolloClient, 
   ApolloProvider, 
   InMemoryCache, 
-  createHttpLink } from '@apollo/client';
+  createHttpLink,
+  split,
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
@@ -28,9 +32,27 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5000/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink),
+);
+
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
